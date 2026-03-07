d3ff6ff240ab52a544d205a39f13b7f2a5a16b34
Toshe <toshe@braingraph.com>
2026-03-07 21:51:52 +0100

    ytruytr

--- api/passenger_wsgi.py
+++ api/passenger_wsgi.py
@@ -1,9 +1,10 @@
-import sys
 import os
+import sys
 
 sys.path.insert(0, os.path.dirname(__file__))
 
 from a2wsgi import ASGIMiddleware
+
 from app.main import app
 
 application = ASGIMiddleware(app)
